039884cad716d9fbebe334d361991cb24f107e7b
Jung <jwork1994212@gmail.com>
2024-03-19 22:28:18 +0800

    add testing borrow and repay

--- src/erc20/MyERC20.sol
+++ src/erc20/MyERC20.sol
@@ -5,12 +5,7 @@ pragma solidity ^0.8.21;
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MyERC20 is ERC20 {
-
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _supply
-    ) ERC20(_name, _symbol) {
+    constructor(string memory _name, string memory _symbol, uint256 _supply) ERC20(_name, _symbol) {
         _mint(msg.sender, _supply);
     }
 
--- test/CompoundTest.t.sol
+++ test/CompoundTest.t.sol
@@ -18,7 +18,9 @@ contract CompoundTest is Test {
     SimplePriceOracle public priceOracle;
     WhitePaperInterestRateModel public interestRateModel;
 
-    uint private constant MINT_AMOUNT = 100;
+    uint256 private constant MINT_AMOUNT = 100;
+    uint256 private constant BORROW_AMOUNT = 50;
+    address private constant USER2 = address(0x22);
 
     function setUp() public {
         comptroller = new Comptroller();
@@ -28,12 +30,12 @@ contract CompoundTest is Test {
         // set simplePriceOracle as price oracle
         comptroller._setPriceOracle(priceOracle);
 
-
+        (tokenA, cTokenA) = createToken("TokenA", "TKA", "cTokenA", "cTKA");
+        (tokenB, cTokenB) = createToken("TokenB", "TKB", "cTokenB", "cTKB");
     }
 
     function test_assert_mint_success() public {
-        createTokenA();
-        mint();
+        mintCToken(tokenA, cTokenA, MINT_AMOUNT);
 
         // cErc20's erc20A increase
         assertEq(tokenA.balanceOf(address(cTokenA)), MINT_AMOUNT);
@@ -43,8 +45,8 @@ contract CompoundTest is Test {
     }
 
     function test_assert_redeem_success() public {
-        createTokenA();
-        redeem();
+        mintCToken(tokenA, cTokenA, MINT_AMOUNT);
+        redeem(cTokenA, MINT_AMOUNT);
 
         // cErc20's erc20A is 0
         assertEq(tokenA.balanceOf(address(cTokenA)), 0);
@@ -54,44 +56,82 @@ contract CompoundTest is Test {
     }
 
     function test_assert_borrow_success() public {
+        setupTokens();
 
+        // mint cTokenB
+        mintCToken(tokenB, cTokenB, 1);
+
+        // That's say this test contract as User1
+        // Before borrow, User1's balance of tokenA and cTokenA's balance of tokenA
+        uint256 user1Balance = tokenA.balanceOf(address(this));
+        uint256 cTokenABalance = tokenA.balanceOf(address(cTokenA));
+
+        // borrow
+        // Maximin of borrow amount is 50$, cuz tokenB's collateral is 50% and we only have 1 tokenB(price 100$)
+        cTokenA.borrow(BORROW_AMOUNT);
+
+        // After borrow
+        // User1's balance of tokenA should increase 50(BORROW_AMOUNT)
+        assertEq(tokenA.balanceOf(address(this)), user1Balance + BORROW_AMOUNT);
+        // cTokenA's balance of tokenA should decrease 50(BORROW_AMOUNT)
+        assertEq(tokenA.balanceOf(address(cTokenA)), cTokenABalance - BORROW_AMOUNT);
     }
 
-    function createTokenA() private {
-        tokenA = new MyERC20("TokenA", "TKA", 1e18);
-        cTokenA = new CErc20Immutable(
-            address(tokenA),
-            comptroller,
-            interestRateModel,
-            1e18,
-            "cTokenA",
-            "cTKA",
-            18,
-            payable(address(this))
+    function test_assert_repay_success() public {
+        setupTokens();
+
+        // mint cTokenB
+        mintCToken(tokenB, cTokenB, 1);
+
+        // borrow 50$ tokenA
+        cTokenA.borrow(BORROW_AMOUNT);
+
+        // That's say this test contract as User1
+        // After borrow, User1's balance of tokenA and cTokenA's balance of tokenA
+        uint256 user1Balance = tokenA.balanceOf(address(this));
+        uint256 cTokenABalance = tokenA.balanceOf(address(cTokenA));
+
+        // when cTokenA execute repayBorrow will transfer tokenA from User1 to cTokenA,
+        // so need to approve first
+        tokenA.approve(address(cTokenA), BORROW_AMOUNT);
+        cTokenA.repayBorrow(BORROW_AMOUNT);
+
+        // After repay
+        // User1's balance of tokenA should decrease 50(BORROW_AMOUNT)
+        assertEq(tokenA.balanceOf(address(this)), user1Balance - BORROW_AMOUNT);
+        // cTokenA's balance of tokenA should increase 50(BORROW_AMOUNT)
+        assertEq(tokenA.balanceOf(address(cTokenA)), cTokenABalance + BORROW_AMOUNT);
+    }
+
+    function createToken(string memory _name, string memory _symbol, string memory _cName, string memory _cSymbol)
+        private
+        returns (MyERC20 token, CErc20 cToken)
+    {
+        token = new MyERC20(_name, _symbol, 1e18);
+        cToken = new CErc20Immutable(
+            address(token), comptroller, interestRateModel, 1e18, _cName, _cSymbol, 18, payable(address(this))
         );
 
-        // add cTokenA to supportMarket
-        comptroller._supportMarket(cTokenA);
+        // Add cToken to the markets mapping
+        comptroller._supportMarket(cToken);
     }
 
-    function createTokenB() private {
-        tokenB = new MyERC20("TokenB", "TKB", 1e18);
-        cTokenB = new CErc20Immutable(
-            address(tokenB),
-            comptroller,
-            interestRateModel,
-            1e18,
-            "cTokenB",
-            "cTKB",
-            18,
-            payable(address(this))
-        );
+    function mintCToken(MyERC20 erc20, CErc20 cErc20, uint256 amount) private {
+        // to approve cErc20 address, cuz we will do transferFrom when mint cErc20
+        erc20.approve(address(cErc20), amount);
 
-        // add cTokenB to supportMarket
-        comptroller._supportMarket(cTokenB);
+        // mint cErc20
+        // there are 2 steps in this mint action: do transferFrom in erc20 first, and then mint cErc20
+        cErc20.mint(amount);
     }
 
-    function prepare() private {
+    function redeem(CErc20 cErc20, uint256 amount) private {
+        // do redeem
+        // there are 2 steps in this redeem action: to decrease cErc20 first, then do transfer in erc20
+        cErc20.redeem(amount);
+    }
+
+    function setupTokens() private {
         // set tokenA's price as 1$
         priceOracle.setUnderlyingPrice(cTokenA, 1e18);
         // set tokenB's price as 100$
@@ -102,24 +142,11 @@ contract CompoundTest is Test {
         address[] memory cTokens = new address[](1);
         cTokens[0] = address(cTokenB);
         comptroller.enterMarkets(cTokens);
+
+        // supply tokenA
+        vm.startPrank(USER2);
+        tokenA.mint(MINT_AMOUNT);
+        mintCToken(tokenA, cTokenA, MINT_AMOUNT);
+        vm.stopPrank();
     }
-
-    function mint() private {
-        // to approve cErc20 address, cuz we will do transferFrom when mint cErc20
-        tokenA.approve(address(cTokenA), MINT_AMOUNT);
-
-        // mint cErc20
-        // there are 2 steps in this mint action: do transferFrom in erc20 first, and then mint cErc20
-        cTokenA.mint(MINT_AMOUNT);
-    }
-
-    function redeem() private {
-        // do mint
-        mint();
-
-        // do redeem
-        // there are 2 steps in this redeem action: to decrease cErc20 first, then do transfer in erc20
-        cTokenA.redeem(MINT_AMOUNT);
-    }
-
 }
