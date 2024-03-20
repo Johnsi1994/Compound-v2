caee2d3b3128363862a5dd0db9cccc47699734dd
Jung <jwork1994212@gmail.com>
2024-03-20 10:43:52 +0800

    add testing liquidate by change collateral factor an token price

--- test/CompoundTest.t.sol
+++ test/CompoundTest.t.sol
@@ -20,6 +20,10 @@ contract CompoundTest is Test {
 
     uint256 private constant MINT_AMOUNT = 100;
     uint256 private constant BORROW_AMOUNT = 50;
+    uint256 private constant CLOSE_FACTOR = 0.5 * 1e18;
+    uint256 private constant LIQUIDATION_INCENTIVE = 0.1 * 1e18;
+    uint256 private constant COLLATERAL_FACTOR_30 = 0.3 * 1e18;
+
     address private constant USER2 = address(0x22);
 
     function setUp() public {
@@ -101,6 +105,120 @@ contract CompoundTest is Test {
         assertEq(tokenA.balanceOf(address(this)), user1Balance - BORROW_AMOUNT);
         // cTokenA's balance of tokenA should increase 50(BORROW_AMOUNT)
         assertEq(tokenA.balanceOf(address(cTokenA)), cTokenABalance + BORROW_AMOUNT);
+    }
+
+    function test_change_collateral_factor_then_liquidate_success() public {
+        setupTokens();
+
+        // mint cTokenB
+        mintCToken(tokenB, cTokenB, 1);
+
+        // borrow 50$ tokenA
+        cTokenA.borrow(BORROW_AMOUNT);
+
+        // set close factor to 50%
+        comptroller._setCloseFactor(CLOSE_FACTOR);
+
+        // set liquidation incentive to 10%
+        comptroller._setLiquidationIncentive(LIQUIDATION_INCENTIVE);
+
+        // set tokenB's new collateral factor as 30%
+        comptroller._setCollateralFactor(cTokenB, COLLATERAL_FACTOR_30);
+
+        // check collateral factor been updated
+        (, uint256 collateralFactorMantissa,) = comptroller.markets(address(cTokenB));
+        assertEq(collateralFactorMantissa, COLLATERAL_FACTOR_30);
+
+        // check shortfall is greater then 0, cuz collateral factor has change form 50 to 30
+        // shortfall is 20 at this moment
+        (,, uint256 shortfall) = comptroller.getAccountLiquidity(address(this));
+        assertGt(shortfall, 0);
+
+        uint256 repayAmount = shortfall * CLOSE_FACTOR / 1e18;
+
+        vm.startPrank(USER2);
+        // User2 mint the repay amount of tokenA then approve for later doing liquidate
+        tokenA.mint(repayAmount);
+        tokenA.approve(address(cTokenA), repayAmount);
+
+        // balance before liquidate
+        uint256 user2Balance = tokenA.balanceOf(address(USER2));
+        uint256 cTokenABalance = tokenA.balanceOf(address(cTokenA));
+
+        // User2 liquidate User1, so in tokenA, User2's balance will decrease repayAmount
+        cTokenA.liquidateBorrow(address(this), repayAmount, cTokenB);
+        vm.stopPrank();
+
+        // After liquidate User2's balance decrease repayAmount and cTokenA's balance increase repayAmount
+        assertEq(tokenA.balanceOf(address(USER2)), user2Balance - repayAmount);
+        assertEq(tokenA.balanceOf(address(cTokenA)), cTokenABalance + repayAmount);
+
+        /**
+         * 需要再研究的問題:
+         * 這個測試會成功，但看起來沒有測試到 User2 的收益，總不可能幫忙清算沒酬勞，要研究一下酬勞去哪驗證
+         */
+
+        // console.log("==========================================================");
+        // console.log("TokenA");
+        // console.log("Balance of User 1 : %d", tokenA.balanceOf(address(this)));
+        // console.log("Balance of User 2 : %d", tokenA.balanceOf(address(USER2)));
+        // console.log("Balance of cTokenA: %d", tokenA.balanceOf(address(cTokenA)));
+        // console.log("==========================================================");
+        // console.log("cTokenA");
+        // console.log("Balance of User 1 : %d", cTokenA.balanceOf(address(this)));
+        // console.log("Balance of User 2 : %d", cTokenA.balanceOf(address(USER2)));
+        // console.log("==========================================================");
+        // console.log("TokenB");
+        // console.log("Balance of User 1 : %d", tokenB.balanceOf(address(this)));
+        // console.log("Balance of User 2 : %d", tokenB.balanceOf(address(USER2)));
+        // console.log("Balance of cTokenB: %d", tokenB.balanceOf(address(cTokenB)));
+        // console.log("==========================================================");
+        // console.log("cTokenB");
+        // console.log("Balance of User 1 : %d", cTokenB.balanceOf(address(this)));
+        // console.log("Balance of User 2 : %d", cTokenB.balanceOf(address(USER2)));
+    }
+
+    function test_change_token_price_then_liquidate_success() public {
+        setupTokens();
+
+        // mint cTokenB
+        mintCToken(tokenB, cTokenB, 1);
+
+        // borrow 50$ tokenA
+        cTokenA.borrow(BORROW_AMOUNT);
+
+        // set close factor to 50%
+        comptroller._setCloseFactor(CLOSE_FACTOR);
+
+        // set liquidation incentive to 10%
+        comptroller._setLiquidationIncentive(LIQUIDATION_INCENTIVE);
+
+        // change cTokenB price to 50$
+        priceOracle.setUnderlyingPrice(cTokenB, 50 * 1e18);
+
+        // check shortfall is greater then 0, cuz the price of cTokenB has change form 100 to 50
+        // shortfall is 25 at this moment
+        (,, uint256 shortfall) = comptroller.getAccountLiquidity(address(this));
+        assertGt(shortfall, 0);
+
+        uint256 repayAmount = shortfall * CLOSE_FACTOR / 1e18;
+
+        vm.startPrank(USER2);
+        // User2 mint the repay amount of tokenA then approve for later doing liquidate
+        tokenA.mint(repayAmount);
+        tokenA.approve(address(cTokenA), repayAmount);
+
+        // balance before liquidate
+        uint256 user2Balance = tokenA.balanceOf(address(USER2));
+        uint256 cTokenABalance = tokenA.balanceOf(address(cTokenA));
+
+        // User2 liquidate User1, so in tokenA, User2's balance will decrease repayAmount
+        cTokenA.liquidateBorrow(address(this), repayAmount, cTokenB);
+        vm.stopPrank();
+
+        // After liquidate User2's balance decrease repayAmount and cTokenA's balance increase repayAmount
+        assertEq(tokenA.balanceOf(address(USER2)), user2Balance - repayAmount);
+        assertEq(tokenA.balanceOf(address(cTokenA)), cTokenABalance + repayAmount);
     }
 
     function createToken(string memory _name, string memory _symbol, string memory _cName, string memory _cSymbol)
